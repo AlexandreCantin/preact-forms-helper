0f2ac3ead4df6852d76d5ae546aa7161ec9e33c3
alexandre <cantin.alexandre@gmail.com>
2018-11-04 19:14:08 +0100

    Use native method class

--- src/form/index.js
+++ src/form/index.js
@@ -8,11 +8,11 @@ export default class Form {
     this.touched = {} // Touched fields (all false by default)
 
 
-    for(let inputName in formData) {
+    for (let inputName in formData) {
       let inputData = formData[inputName];
 
       // Values
-      if(inputData['value']) this.values[inputName] = inputData['value'];
+      if (inputData['value']) this.values[inputName] = inputData['value'];
       else this.values[inputName] = inputData.multiple ? [] : null;
 
       // Validators
@@ -29,7 +29,7 @@ export default class Form {
   }
 
   updateValidators(newFormValidators) {
-    for(let inputName in newFormValidators) {
+    for (let inputName in newFormValidators) {
       this.checkFieldExists(inputName);
 
       let inputData = newFormValidators[inputName];
@@ -41,17 +41,17 @@ export default class Form {
   saveField(name, value) {
     this.touched[name] = true;
 
-    if(Array.isArray(this.values[name])) {
-      if(Array.isArray(value)) {
+    if (Array.isArray(this.values[name])) {
+      if (Array.isArray(value)) {
         // Case 'select.multiple' => we got an array
         this.values[name] = value;
       } else {
         // Checkboxes : we add/remove value one by one
-        if(this.values[name].includes(value)) this.values[name] = this.values[name].filter(v => v !== value)
+        if (this.values[name].includes(value)) this.values[name] = this.values[name].filter(v => v !== value)
         else this.values[name].push(value);
       }
     }
-    else if(Array.isArray(value)) {
+    else if (Array.isArray(value)) {
       // Array values on a non-multiple field => error
       throw new Error('Can\'t set an array value to a non-mulitple field. Please add `multiple:true` for \'' + name + '\' when calling Form constructor');
     } else {
@@ -65,13 +65,13 @@ export default class Form {
   validateField(name, value, forceUpdateFn) {
     // Extract validators
     let validators = this.validators[name];
-    if(!validators) throw new Error('No entry  \'' + name + '\' in the given form description.')
+    if (!validators) throw new Error('No entry  \'' + name + '\' in the given form description.')
 
     // Remove the current errors
     delete this.errors[name];
 
     validators.forEach(v => {
-      if(!v.isValid(value)) {
+      if (!v.isValid(value)) {
         let errorsArray = this.errors[name] || [];
         errorsArray.push(v.errorName)
         this.errors[name] = errorsArray;
@@ -81,51 +81,51 @@ export default class Form {
     this.updateFormStatus();
 
     // Call forceUpdate()
-    if(forceUpdateFn) forceUpdateFn();
+    if (forceUpdateFn) forceUpdateFn();
   }
 
-  checkFieldExists = (key) => {
-    if(this.validators[key] === undefined) throw new Error('Unknown field: \'' + key + '\'');
+  checkFieldExists(key) {
+    if (this.validators[key] === undefined) throw new Error('Unknown field: \'' + key + '\'');
     return true;
   }
 
-  updateFormStatus = () => {
+  updateFormStatus() {
     this.valid = Object.keys(this.errors).length === 0;
   }
 
   /* Return true if the given field has at least one error */
-  hasErrors = (fieldName) => { this.checkFieldExists(fieldName); return this.errors[fieldName] !== undefined; }
+  hasErrors(fieldName) { this.checkFieldExists(fieldName); return this.errors[fieldName] !== undefined; }
 
   /* Return all the errors associated to the given field */
-  getErrors = (fieldName) => { this.checkFieldExists(fieldName); return this.errors[fieldName] || []; }
+  getErrors(fieldName) { this.checkFieldExists(fieldName); return this.errors[fieldName] || []; }
 
   /* Return if a field has been modified by an user */
-  isTouched = (fieldName) => { this.checkFieldExists(fieldName); return this.touched[fieldName]; }
+  isTouched(fieldName) { this.checkFieldExists(fieldName); return this.touched[fieldName]; }
 
   /* Return true if the given field has the given error */
-  hasError = (fieldName, errorName) => {
+  hasError(fieldName, errorName) {
     this.checkFieldExists(fieldName);
-    if(!this.errors[fieldName]) return false;
+    if (!this.errors[fieldName]) return false;
     return this.errors[fieldName].indexOf(errorName) !== -1;
   }
 
-  isSelected = (fieldName, value) => {
+  isSelected(fieldName, value) {
     this.checkFieldExists(fieldName);
     let savedValue = this.values[fieldName];
     return Array.isArray(savedValue) ? savedValue.includes(value) : savedValue === value;
   }
 
-  getValue = (fieldName) => {
+  getValue(fieldName) {
     this.checkFieldExists(fieldName);
     return this.values[fieldName];
   }
 
-  setValue = (fieldName, value, component) => {
+  setValue(fieldName, value, component) {
     this.checkFieldExists(fieldName);
     this.values[fieldName] = value;
     this.validateField(fieldName, value, component ? () => component.forceUpdate() : undefined);
   }
 
-  getValues = () => this.values;
-  isValid = () => this.valid;
+  getValues() { return this.values; }
+  isValid() { return this.valid; }
 }
